812021c416b25389f8b994e096cb7b1fcc251acf
Vandenborne Stefan <stefan@illutek.be>
2014-10-26 20:35:01 +0100

    op date 26102014

---
+++ templates/node--foto_album.tpl.php
@@ -0,0 +1,10 @@
+<?php if ($teaser): ?>
+    <div class="foto"><a href="<?php print $node_url; ?>"><?php print render($content['field_foto'][rand(0, count($field_foto) - 1)]); ?></a></div>
+<?php else: ?>
+
+<!-- hier de volledige node -->
+
+    <h2><?php print $title; ?></h2>
+    <div class="foto"><?php print render($content['field_foto']); ?></div>
+
+<?php endif; ?>
